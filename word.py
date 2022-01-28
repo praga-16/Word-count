b7fc497cad6cc811fcbd125dfc246af3db4c69a9
praga016 <pragatheesvaran1972@gmail.com>
2022-01-28 16:21:56 +0530

    code

---
+++ word.py
@@ -0,0 +1,10 @@
+Developed by pragtheesvaran ab
+Reference no 21003592
+To write a program for getting the word count from a file...
+
+num_of_words = 0 
+file = open('text.txt')
+text = file.read()
+words = text.split()
+num_of_words = len(words)
+print("Number of words = ",num_of_words)   
